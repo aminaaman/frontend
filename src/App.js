edfec2dbbe84a9039589598673ef81629660c718
Akbotam <bmautkazy@gmail.com>
2020-04-20 20:29:09 +0600

    auth part

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import './css/App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import 'antd/dist/antd.css';
+import * as actions from './store/actions/auth';
 
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Footer/>
-    </div>
-  );
+class  App extends Component {
+
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header {...this.props} />
+        <Footer/>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
+import reducer from './store/reducers/auth';
 
-ReactDOM.render(
-  <React.StrictMode>
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhances(
+    applyMiddleware(thunk)
+));
+
+const app = (
+  <Provider store = {store}> 
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
